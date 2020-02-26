68a0b029ab913ef9b97b8603351ceccf42468794
iachievedream <iachievedream@gmail.com>
2020-02-26 18:09:55 +0800

    dd() input something

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            // cc(Auth::guard($guard)->check());die;
+            dd(Auth::guard($guard)->check());
             return redirect(RouteServiceProvider::HOME);
         }
         // 執行前動作
--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/','ArticleController@index');
-// Route::get('/create','ArticleController@create');
+Route::get('/create','ArticleController@create');
