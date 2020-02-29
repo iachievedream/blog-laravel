e616a1e66183b56a77b918154d1f67737bbbde26
iachievedream <iachievedream@gmail.com>
2020-02-29 21:57:59 +0800

    last authority management

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,13 +3,13 @@
 	<a href="{{route('register')}}">註冊</a>
 @else
 	<form method="POST" action="{{route('logout')}}">
-		使用者為：{{Auth::user()->name}}
-		<button type="submin">登出</button>
 		@csrf
+		使用者為：{{Auth::user()->name}}
+		<button type="submit">登出</button>
 	</form>
 @endguest
 
-<a href="create">新增文章</a><br>
+<a href="{{route('create')}}">新增文章</a><br>
 文章列表<br>
 標題--作者<br>
 @foreach($article as $article)
