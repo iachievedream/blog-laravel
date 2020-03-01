201803adb34ca7c960a77f7174c8f8ab3d58f7d7
iachievedream <iachievedream@gmail.com>
2020-03-01 12:23:56 +0800

    validation is finish in store and update

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            dd(Auth::guard($guard)->check());
+            // return("abc");
+            // dd(Auth::guard($guard)->check());
             return redirect(RouteServiceProvider::HOME);
         }
         // 執行前動作
