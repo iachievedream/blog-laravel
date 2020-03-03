e8aec91474dddfe44142ce2c5fc6b50806786a78
iachievedream <iachievedream@gmail.com>
2020-03-03 11:48:10 +0800

    fix finish

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,8 +4,8 @@
     進入身分：訪客<br>
 @endif
 
-<a href="edit/{{$article->id}}">編輯</a>
-<form action="delete/{{$article->id}}" method="POST">
+<a href="edit/{{$articles->id}}">編輯</a>
+<form action="delete/{{$articles->id}}" method="POST">
     @csrf
     @method('DELETE')
     <button type="submit">刪除</button>
@@ -13,6 +13,6 @@
 <a href="/">上一頁</a><br>
 單一文章列表<br>
 標題---內容---作者<br>
-{{$article->title}}---
-{{$article->content}}---
-{{$article->author}}
+{{$articles->title}}---
+{{$articles->content}}---
+{{$articles->author}}
