f166ef019d4faa83a6813afd861f03fd645b3b78
iachievedream <iachievedream@gmail.com>
2020-04-20 00:10:49 +0800

    add notebook

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +11,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' =>'root',
+            'email' => 'root'.'@gmail.com',
+            'password' => Hash::make('root'),
+            'role' => 'root',
+        ]);
     }
 }
